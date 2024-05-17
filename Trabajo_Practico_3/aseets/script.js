9d3b0da346fbcf7b03f3f13f614e03fabf6aec96
Felix Polo <brunolok@gmail.com>
2024-05-17 14:14:10 -0300

    se dio formato mas prolijo al documento js

--- Trabajo_Practico_3/aseets/script.js
+++ Trabajo_Practico_3/aseets/script.js
@@ -1,63 +1,74 @@
 let opciones;
 let tareas = [];
-function seleccion(){
-opciones = parseInt(
-  prompt(
-    "que operacion decea realizar \n 1 Agregar Tarea\n 2 Mostra Lista De Tarea\n 3 Editar Tarea \n 4 Eliminar Tarea\n 0 Salir\n"
-  )
-);
+function seleccion() {
+  opciones = parseInt(
+    prompt(
+      "que operacion decea realizar \n 1 Agregar Tarea\n 2 Mostra Lista De Tarea\n 3 Editar Tarea \n 4 Eliminar Tarea\n 0 Salir\n"
+    )
+  );
 }
-while (opciones != 0){
-    seleccion();
-  function agregarTareas(){
+while (opciones != 0) {
+  seleccion();
+  function agregarTareas() {
     tareas.push(prompt("ingrese su tarea nueva"));
     if (tareas != 0) {
-      alert("-Tarea nueva añadida- \n" + tareas.join('\n'));
+      alert("-Tarea nueva añadida- \n" + tareas.join("\n"));
     } else {
       alert("no ingresate ninguna tarea");
     }
   }
-  function mostraTareas(){
-    if(tareas != 0){
-        alert('-sus tareas son-\n'+tareas.join('\n'));
-    }else{
-        alert('no agregaste ninguna tarea todavia');
+  function mostraTareas() {
+    if (tareas != 0) {
+      alert("-sus tareas son-\n" + tareas.join("\n"));
+    } else {
+      alert("no agregaste ninguna tarea todavia");
     }
   }
-  function edicioTareas(){
-    if (tareas != 0){
-    tareas[parseInt(prompt('Selecione el elemento a modificar.\nDe arriba abajo constando desde el 0 que es el primero arriba:\n\n'+tareas.join('\n')))] = [prompt('ingresa la nueva tarea')];
-    alert('-se actualizo sus tareas-\n' + tareas.join('\n'));
-    }else{
-        alert('no hay tareas para modificar');
+  function edicioTareas() {
+    if (tareas != 0) {
+      tareas[
+        parseInt(
+          prompt(
+            "Selecione el elemento a modificar.\nDe arriba abajo constando desde el 0 que es el primero arriba:\n\n" +
+              tareas.join("\n")
+          )
+        )
+      ] = [prompt("ingresa la nueva tarea")];
+      alert("-se actualizo sus tareas-\n" + tareas.join("\n"));
+    } else {
+      alert("no hay tareas para modificar");
     }
   }
-  function eliminarTareas(){
-    if(tareas != 0){
-        tareas.shift([parseInt(prompt('Selecione el elemento a eliminar.\n De arriba abajo constando desde el 0 que es el primero arriba:\n\n'+tareas.join('\n')))]);
-    alert('-se a eliminado el elemento-\n' + tareas.join('\n'));
-    }else{
-        alert('no hay tareas para eliminar');
+  function eliminarTareas() {
+    if (tareas != 0) {
+      tareas.shift([
+        parseInt(
+          prompt(
+            "Selecione el elemento a eliminar.\n De arriba abajo constando desde el 0 que es el primero arriba:\n\n" +
+              tareas.join("\n")
+          )
+        ),
+      ]);
+      alert("-se a eliminado el elemento-\n" + tareas.join("\n"));
+    } else {
+      alert("no hay tareas para eliminar");
     }
-    
   }
-  
 
-    switch (opciones){
+  switch (opciones) {
     case 1:
       agregarTareas();
       break;
     case 2:
-        mostraTareas();
+      mostraTareas();
       break;
     case 3:
-        edicioTareas();
+      edicioTareas();
       break;
     case 4:
-        eliminarTareas();
-        break;
+      eliminarTareas();
+      break;
     case 0:
-        alert('adios que tenga un buen dia ');
+      alert("adios que tenga un buen dia ");
   }
- }
-
+}
